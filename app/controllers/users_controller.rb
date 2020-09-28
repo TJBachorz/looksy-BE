7f0567725c894cb1d63e36a661f8c244a9055031
TJBachorz <TJBachorz@gmail.com>
2020-09-28 15:37:52 -0600

    adds auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     before_action :find_user_by_id, only: [:show, :update, :destroy]
-    # skip_before_action :authorized, only: [:login, :create]
+    skip_before_action :authorized, only: [:login, :create]
 
     def index 
         @users = User.all
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
             @user.save
             render json: @user
         else 
-            render json: user.errors.full_messages
+            render json: @user.errors.full_messages
         end
     end
 
@@ -31,7 +31,14 @@ class UsersController < ApplicationController
     end
 
     def login
-
+        @user = User.find_by(username: params[:username])
+        
+        if @user && @user.authenticate(params[:password])
+            @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+            render json: {user: @user, token: @token}
+        else
+            render json: {message: "Invalid credentials!"}
+        end
     end
 
 
