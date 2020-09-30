3f55fed95a387836aa24daa683dc1941dad27f99
TJBachorz <TJBachorz@gmail.com>
2020-09-30 09:17:08 -0600

    attempts to fix heroku backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,9 +32,9 @@ class UsersController < ApplicationController
 
     def login
         @user = User.find_by(username: params[:username])
-        
+    
         if @user && @user.authenticate(params[:password])
-            @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+            @token = JWT.encode({user_id: @user.id}, ENV['SECRET_KEY_BASE'])
             render json: {user: @user, token: @token}
         else
             render json: {message: "Invalid credentials!"}
